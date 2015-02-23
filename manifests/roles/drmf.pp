fda4515dd651e4b624531ef1df34bcea03656ab3
Moritz Schubotz <github@ckurs.de>
2015-02-23 14:10:17 -0500

    Fix: Update array syntax from php to puppet style

--- manifests/roles/drmf.pp
+++ manifests/roles/drmf.pp
@@ -11,7 +11,7 @@ class role::drmf {
       'wgLaTeXMLServer'       => 'http://gw125.iu.xsede.org:8888',
       'wgMathDefaultLaTeXMLSetting[\'preload\'][]' => 'DLMFmath.sty',
       'wgCapitalLinks' => false,
-      'wgHooks[\'MathFormulaRendered\']' => 'array (\'wfOnMathFormulaRendered\')'
+      'wgHooks[\'MathFormulaRendered\']' => [ 'wfOnMathFormulaRendered' ]
     },
   }
 
